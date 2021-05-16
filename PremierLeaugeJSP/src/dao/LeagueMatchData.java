f2b550fc33e59961fcffa43f8e91c23b2abf6810
Woojin <sam4woojin@naver.com>
2021-05-16 11:33:09 +0900

    Fix Recent Game Date

--- PremierLeaugeJSP/src/dao/LeagueMatchData.java
+++ PremierLeaugeJSP/src/dao/LeagueMatchData.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Date;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -24,9 +25,62 @@ private static JSONArray matchArray = new JSONArray();
 	}
 	
 	public leagueMatchData() throws IOException, InterruptedException {
-
+		
+		String date = new Date().toString();
+		// System.out.println(date);
+		
+		String dateArr[] = date.split(" ");
+		String month = dateArr[1];
+		String day = dateArr[2];
+		String year = dateArr[5];
+	
+		String monthNumber = null;
+		
+		switch(month) {
+		case "January": 
+			monthNumber = "01";
+			break;
+		case "February": 
+			monthNumber = "02";
+			break;
+		case "March": 
+			monthNumber = "03";
+			break;
+		case "April": 
+			monthNumber = "04";
+			break;
+		case "May": 
+			monthNumber = "05";
+			break;
+		case "June": 
+			monthNumber = "06";
+			break;
+		case "July": 
+			monthNumber = "07";
+			break;
+		case "August": 
+			monthNumber = "08";
+			break;
+		case "September": 
+			monthNumber = "09";
+			break;
+		case "October": 
+			monthNumber = "10";
+			break;
+		case "November": 
+			monthNumber = "11";
+			break;
+		case "December": 
+			monthNumber = "12";
+			break;
+		}
+		
+		String todayDate = day + "." + monthNumber + "." + year;
+		System.out.println(todayDate);
+		
+		
 		HttpRequest request = HttpRequest.newBuilder()
-				.uri(URI.create("https://data.football-api.com/v3/matches?comp_id=1204&from_date=30.04.2021&to_date=07.05.2021&Authorization=cfnR6LWc4i4MDFLlPJrajoa465c4qjF594kpIy4b"))
+				.uri(URI.create("https://data.football-api.com/v3/matches?comp_id=1204&from_date=30.04.2021&to_date=" + todayDate + "&Authorization=cfnR6LWc4i4MDFLlPJrajoa465c4qjF594kpIy4b"))
 				.method("GET", HttpRequest.BodyPublishers.noBody())
 				.build();
 		HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
@@ -43,14 +97,13 @@ private static JSONArray matchArray = new JSONArray();
 		System.out.println(matchArray);
 		
 		for(int i=matchArray.size()-1 ; i>=0 ; i--){
-		 	JSONObject temp2Obj = (JSONObject) matchArray.get(i);
-		 	
-		 	System.out.println(temp2Obj.get("formatted_date"));
-            System.out.println(temp2Obj.get("visitorteam_name"));
-            System.out.println(temp2Obj.get("localteam_name"));
-            System.out.println(temp2Obj.get("ft_score"));
-            System.out.println(temp2Obj.get("time"));
-            System.out.println("----------------------------");
+		 	JSONObject temp2Obj = (JSONObject) matchArray.get(i);		 	
+//		 	System.out.println(temp2Obj.get("formatted_date"));
+//          System.out.println(temp2Obj.get("visitorteam_name"));
+//          System.out.println(temp2Obj.get("localteam_name"));
+//          System.out.println(temp2Obj.get("ft_score"));
+//          System.out.println(temp2Obj.get("time"));
+//          System.out.println("----------------------------");
 	 }
 
         setMatchArray(matchArray);
